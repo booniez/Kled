2ccd6a6f4561f86188f704c53f795de77797df09
JLM <yuanl@ccwcar.com>
2019-12-25 17:59:17 +0800

    initial

---
+++ Example/Pods/Target Support Files/Kled/Kled-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Kled : NSObject
+@end
+@implementation PodsDummy_Kled
+@end
---
+++ Example/Pods/Target Support Files/Pods-Kled_Example/Pods-Kled_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kled_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Kled_Example
+@end
